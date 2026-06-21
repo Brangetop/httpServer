4863a2f377cd7abd54c3a1df5b9b8b1fbe82e16d
Brangetop <brangetop360@gmail.com>
2026-06-21 19:03:41 +0300

    Added handlers

--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	str := "Hellow, World!"
+	b := []byte(str)
 
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("Error while writing http response", err.Error())
+	} else {
+		fmt.Println("Success!")
+	}
 }
+
+func payHandler(w http.ResponseWriter, r *http.Request) {
+	str := "Hellow, World!"
+	b := []byte(str)
+
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("Error while writing http response", err.Error())
+	} else {
+		fmt.Println("Payment succeded. Success!")
+	}
+}
+
+func cancelHandler(w http.ResponseWriter, r *http.Request) {
+	str := "Hellow, World!"
+	b := []byte(str)
+
+	_, err := w.Write(b)
+	if err != nil {
+		fmt.Println("Error while writing http response", err.Error())
+	} else {
+		fmt.Println("Payment cancelled. Success!")
+	}
+}
+
 func main() {
-	http.HandleFunc("", handler)
+	http.HandleFunc("/default", handler)
+	http.HandleFunc("/pay", payHandler)
+	http.HandleFunc("/cancel", cancelHandler)
 }
